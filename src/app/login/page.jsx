29d63b32491dd4cd451a42e4fd3e5a9a1c798f35
CodeByMonir <monirhossen39682@gmail.com>
2026-05-02 11:06:09 +0600

    commit

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -4,8 +4,6 @@ const page = () => {
     return (
         <div>
             <h1>Login Page</h1>
-            this is the login page.
-            login to access the dashboard.
         </div>
     );
 };
