6e02421d339af62bcf1a90bf53fd4ad814a48fa7
CodeByMonir <monirhossen39682@gmail.com>
2026-05-03 00:02:35 +0600

    finding

---
+++ src/app/login/SignInPage.jsx
@@ -0,0 +1,143 @@
+"use client";
+
+import { authClient } from "@/lib/auth-client";
+import { Check } from "@gravity-ui/icons";
+import {
+    Button,
+    Card,
+    Description,
+    FieldError,
+    Form,
+    Input,
+    Label,
+    TextField,
+} from "@heroui/react";
+import { useRouter } from "next/navigation";
+import { FcEmptyTrash } from "react-icons/fc";
+import { GrGoogle } from "react-icons/gr";
+
+export default function SignInPage({ searchParams }) {
+
+    const router = useRouter();
+
+    // Get redirect path
+    const redirect = searchParams?.redirect;
+
+    const onSubmit = async (e) => {
+
+        e.preventDefault();
+
+        const email = e.target.email.value;
+        const password = e.target.password.value;
+
+        const { data, error } = await authClient.signIn.email({
+            email,
+            password,
+        });
+
+        if (error) {
+            console.log(error);
+            return;
+        }
+
+        // Redirect back after login
+        router.push(redirect || "/");
+
+        console.log(data);
+    };
+
+    const handleGoogleSignIn = async () => {
+
+        await authClient.signIn.social({
+            provider: 'google',
+
+            // Redirect after Google login
+            callbackURL: redirect || "/",
+        })
+
+    }
+
+
+
+    return (
+        <Card className="border mx-auto w-125 py-10 mt-5">
+            <h1 className="text-center text-2xl font-bold">Log In</h1>
+
+            <Form className="flex w-96 mx-auto flex-col gap-4" onSubmit={onSubmit}>
+                <TextField
+                    isRequired
+                    name="email"
+                    type="email"
+                    validate={(value) => {
+                        if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+                            return "Please enter a valid email address";
+                        }
+
+                        return null;
+                    }}
+                >
+                    <Label>Email</Label>
+                    <Input placeholder="john@example.com" />
+                    <FieldError />
+                </TextField>
+
+                <TextField
+                    isRequired
+                    minLength={8}
+                    name="password"
+                    type="password"
+                    validate={(value) => {
+                        if (value.length < 8) {
+                            return "Password must be at least 8 characters";
+                        }
+                        if (!/[A-Z]/.test(value)) {
+                            return "Password must contain at least one uppercase letter";
+                        }
+                        if (!/[0-9]/.test(value)) {
+                            return "Password must contain at least one number";
+                        }
+
+                        return null;
+                    }}
+                >
+                    <Label>Password</Label>
+                    <Input placeholder="Enter your password" />
+                    <Description>
+                        Must be at least 8 characters with 1 uppercase and 1 number
+                    </Description>
+                    <FieldError />
+                </TextField>
+
+                <div className="flex gap-2">
+                    <Button type="submit">
+                        Submit
+                        <Check />
+                    </Button>
+
+                    <Button type="reset" variant="secondary">
+                        Reset
+                        <FcEmptyTrash />
+                    </Button>
+                </div>
+            </Form>
+
+            <p className="text-center">Or</p>
+
+            <Button
+                onClick={handleGoogleSignIn}
+                variant="outline"
+                className={'w-full'}
+            >
+                <GrGoogle />
+                Log In With Google
+            </Button>
+
+            <p className="text-center">
+                Don't have an account?{' '}
+                <a href="/signup" className="text-blue-500 hover:underline">
+                    Sign up
+                </a>
+            </p>
+        </Card>
+    );
+}
