cb1274c854b6e54f8cce78c78c069919893236b9
CodeByMonir <monirhossen39682@gmail.com>
2026-05-02 09:53:30 +0600

    fix deployment issue

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -4,7 +4,8 @@ const page = () => {
     return (
         <div>
             <h1>Login Page</h1>
-            this is the login page
+            this is the login page.
+            login to access the dashboard.
         </div>
     );
 };
