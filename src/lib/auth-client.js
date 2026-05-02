44c5be637f134e1a128d2dfed2088732f595eb08
CodeByMonir <monirhossen39682@gmail.com>
2026-05-02 23:44:12 +0600

    auth client base url changed

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -1,5 +1,5 @@
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-  baseURL: process.env.BETTER_AUTH_URL,
+  baseURL: "https://suncart-mall.vercel.app",
 });
 export const { signIn, signUp, useSession } = createAuthClient();
