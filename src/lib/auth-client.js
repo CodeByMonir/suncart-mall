b7bf0fb9e6d20932674342b7e8f1537f65a0bdc7
CodeByMonir <monirhossen39682@gmail.com>
2026-05-02 23:46:27 +0600

    checking

--- src/lib/auth-client.js
+++ src/lib/auth-client.js
@@ -2,4 +2,3 @@ import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
   baseURL: "https://suncart-mall.vercel.app",
 });
-export const { signIn, signUp, useSession } = createAuthClient();
