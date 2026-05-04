40007f2ffaafa3bb9d320a114967df861097e468
CodeByMonir <monirhossen39682@gmail.com>
2026-05-04 20:34:45 +0600

    still not working the redirction to product details

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -43,9 +43,9 @@ function SignInForm() {
             toast.error(error.message);
         }
         if (data) {
-            toast.success(`Congrats ${data.user.name} ! Log in successful`);
             router.refresh();
-            router.push(redirect || "/");
+            router.replace(redirect || "/");
+            toast.success(`Congrats ${data.user.name} ! Log in successful`);
         }
 
     };
